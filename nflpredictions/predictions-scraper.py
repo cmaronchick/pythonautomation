51bbe1b03739a08f58cecdc174fa06bd75849545
Chris Aronchick <chrisaronchick@gmail.com>
2024-12-19 11:04:44 -0800

    Added espn scraper

--- nflpredictions/predictions-scraper.py
+++ nflpredictions/predictions-scraper.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import csv, traceback, array
 from scraper_nfl import fetch_nfl_data
 from scraper_usatoday import fetch_usatoday_data
+from scraper_espn import fetch_espn_data
 
 
 weeknum = 15
@@ -127,7 +128,7 @@ usatoday = {
 }
 
 espn = {
-    'url': 'https://www.espn.com/nfl/story/_/page/nflviewguide-42531064/nfl-week-14-picks-schedule-fantasy-football-odds-injuries-stats-2024'
+    'url': 'https://www.espn.com/nfl/story/_/page/nflviewguide-42937521/nfl-week-15-picks-schedule-fantasy-football-odds-injuries-stats-2024'
     # https://www.usatoday.com/sports/nfl/
 }
 
@@ -354,60 +355,9 @@ try:
         rows.append(['VinnieIyer',winner, int(winnerScore), loser, int(loserScore)])
 
 
-    # espn formatting
-    driver.get(espn["url"])
-    wait = WebDriverWait(driver, timeout=2)
-    driver.implicitly_wait(10)
-    # resultsTable = driver.find_elements_by_xpath("//*[contains(text(), " + writer['searchTerm'] + ")]")
-    # wait.until(lambda d : resultsTable.is_displayed())
-    gamesBody = driver.find_element(By.CLASS_NAME, "article-body")
-    picksGraph = gamesBody.find_elements(By.XPATH, "//*[contains(text(), 'pick:')]/parent::*")  
-    # response = requests.get()
-    # print(response)
-    # soup = BeautifulSoup(response.text, 'html.parser')
-    # games = soup.find_all('h3')
-    # picks = soup.find_all('strong', string="Pick: ") #, attrs={'class': 'Article-content'}
-
-    # print([t.parent.text for t in soup.findAll('strong', string="Projected score")])
-    print('ESPN picks:', len(picksGraph))
-    picks = []
-    for graphs in picksGraph:
-        splitPicks = graphs.text.split("\n")
-        # print('splitPicks:', splitPicks)
-        for pick in splitPicks:
-            picks.append(pick)
-    print('picks:', len(picks))
-    for predictionString in picks:
-        if predictionString.find("FPI") > -1:
-            continue
-        separatorString = ", "
-        separator = predictionString.find(separatorString)
-        if separator == -1:
-            separatorString = " over "
-            separator = predictionString.find(separatorString)
-        separatorLength = len(separatorString)
-        colonIndex = predictionString.find(":")
-        addedSpaces = 2
-        if colonIndex == -1:
-            colonIndex = 0
-            addedSpaces = 0
-        lastSpace = None
-        winningTeam = None
-        losingTeam = None
-        apostrophe = predictionString.find("'s")
-        writer = predictionString[:apostrophe]
-        print('writer:', writer)
-        firstSpace = predictionString.find(" ",colonIndex+2)
-        print('firstSpace: ', firstSpace)
-        winningTeam = predictionString[colonIndex+2:firstSpace].strip()
-        winnerScore = predictionString[firstSpace+1:separator]
-        lastSpace = predictionString.rfind(" ")
-        losingTeam = predictionString[separator+separatorLength:lastSpace].strip()
-        loserScore = predictionString[lastSpace+1:].strip()
-        print('winningTeam, winningScore, losingScore', winningTeam, winnerScore, losingTeam, loserScore)
-        
-        print([writer + 'ESPN',winningTeam, winnerScore, losingTeam, loserScore])
-        rows.append([writer + 'ESPN',winningTeam, winnerScore, losingTeam, loserScore])
+    espnrows = fetch_espn_data(weeknum, espn['url'])
+    for espnrow in espnrows:
+        rows.append(espnrow)
 
     # dimers formatting
     driver.get('https://www.dimers.com/bet-hub/nfl/schedule')
---
+++ nflpredictions/scraper_espn.py
@@ -0,0 +1,79 @@
+import requests, sys, datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Setup the Chrome WebDriver
+
+chrome_driver_path = './chromedriver'
+
+service = Service(chrome_driver_path)
+weboptions = webdriver.ChromeOptions()
+weboptions.accept_insecure_certs = True
+driver = webdriver.Chrome(options=weboptions)
+
+# espn formatting
+def fetch_espn_data(weeknum, url):
+    espnrows = []
+    try: 
+        driver.get(url)
+        wait = WebDriverWait(driver, timeout=2)
+        driver.implicitly_wait(10)
+        # resultsTable = driver.find_elements_by_xpath("//*[contains(text(), " + writer['searchTerm'] + ")]")
+        # wait.until(lambda d : resultsTable.is_displayed())
+        gamesBody = driver.find_element(By.CLASS_NAME, "article-body")
+        picksGraph = gamesBody.find_elements(By.XPATH, "//*[contains(text(), 'pick:')]/parent::*")  
+        # response = requests.get()
+        # print(response)
+        # soup = BeautifulSoup(response.text, 'html.parser')
+        # games = soup.find_all('h3')
+        # picks = soup.find_all('strong', string="Pick: ") #, attrs={'class': 'Article-content'}
+
+        # print([t.parent.text for t in soup.findAll('strong', string="Projected score")])
+        print('ESPN picks:', len(picksGraph))
+        picks = []
+        for graphs in picksGraph:
+            splitPicks = graphs.text.split("\n")
+            # print('splitPicks:', splitPicks)
+            for pick in splitPicks:
+                picks.append(pick)
+        print('picks:', len(picks))
+        for predictionString in picks:
+            if predictionString.find("FPI") > -1:
+                continue
+            separatorString = ", "
+            separator = predictionString.find(separatorString)
+            if separator == -1:
+                separatorString = " over "
+                separator = predictionString.find(separatorString)
+            separatorLength = len(separatorString)
+            colonIndex = predictionString.find(":")
+            addedSpaces = 2
+            if colonIndex == -1:
+                colonIndex = 0
+                addedSpaces = 0
+            lastSpace = None
+            winningTeam = None
+            losingTeam = None
+            apostrophe = predictionString.find("'s")
+            writer = predictionString[:apostrophe]
+            print('writer:', writer)
+            firstSpace = predictionString.find(" ",colonIndex+2)
+            print('firstSpace: ', firstSpace)
+            winningTeam = predictionString[colonIndex+2:firstSpace].strip()
+            winnerScore = predictionString[firstSpace+1:separator]
+            lastSpace = predictionString.rfind(" ")
+            losingTeam = predictionString[separator+separatorLength:lastSpace].strip()
+            loserScore = predictionString[lastSpace+1:].strip()
+            print('winningTeam, winningScore, losingScore', winningTeam, winnerScore, losingTeam, loserScore)
+            
+            print([writer + 'ESPN',winningTeam, winnerScore, losingTeam, loserScore])
+            espnrows.append([writer + 'ESPN',winningTeam, winnerScore, losingTeam, loserScore])
+        return espnrows
+    except Exception as e:
+        print('espn exception: ', e)
+        return espnrows
